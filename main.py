4383d937e5f58c2c9bef0ec43b648154c8788529
7bvcxz <7bvcxz@gmail.com>
2021-05-13 17:45:48 +0900

    hey you!

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import torch.nn.utils.prune as prune
 import torchvision
 from torchvision import datasets, transforms
 import utils
 from utils import *
 from tqdm import tqdm
-
+import pickle
 
 ##### train, test, retrain function #######################################################
 def train(args, model, device, pattern_set, train_loader, test_loader, optimizer, Z, Y, U, V):
@@ -62,51 +63,43 @@ def test(args, model, device, test_loader):
 
 
 def retrain(args, model, mask, device, train_loader, test_loader, optimizer):
-    best_prec = 0 
-    for epoch in range(args.re_epoch):
-        print('Re epoch: {}'.format(epoch+1))
-        model.train()
-        for batch_idx, (data, target) in enumerate(tqdm(train_loader)):
-            data, target = data.to(device), target.to(device)
-            optimizer.zero_grad()
-            output = model(data)
-            #loss = regularized_nll_loss(args, model, F.log_softmax(output, dim=1), target)
-            loss = regularized_nll_loss(args, model, output, target)
-            loss.backward()
-            optimizer.prune_step(mask)
-        prec = test(args, model, device, test_loader)
-
-        if prec > best_prec:
-            best_prec = prec
-            torch.save(model.state_dict(), os.path.join(args.save, 'cifar10_pruned.pth.tar'))
+    model.train()
+    for batch_idx, (data, target) in enumerate(tqdm(train_loader)):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        #loss = regularized_nll_loss(args, model, F.log_softmax(output, dim=1), target)
+        loss = regularized_nll_loss(args, model, output, target)
+        loss.backward()
+        optimizer.prune_step(mask)
 
 
 ##### Settings #########################################################################
 parser = argparse.ArgumentParser(description='Pytorch PatDNN training')
 parser.add_argument('--model',      default='vgg16_bn',         help='select model')
-parser.add_argument('--dir',        default='~/data',           help='data root')
+parser.add_argument('--dir',        default='~/data',           help='dataset root')
 parser.add_argument('--dataset',    default='cifar10',          help='select dataset')
 parser.add_argument('--batchsize',  default=256, type=int,      help='set batch size')
-parser.add_argument('--lr',         default=3e-4, type=float,   help='set learning rate')
+parser.add_argument('--lr',         default=3e-5, type=float,   help='set learning rate')
+parser.add_argument('--re_lr',      default=1e-4, type=float,   help='set fine learning rate')
 parser.add_argument('--alpha',      default=5e-4, type=float,   help='set l2 regularization alpha')
 parser.add_argument('--adam_epsilon', default=1e-8, type=float, help='adam epsilon')
 parser.add_argument('--rho',        default=1e-2, type=float,   help='set rho')
 parser.add_argument('--connect_perc',  default=3.6, type=float, help='connectivity pruning ratio')
 parser.add_argument('--epoch',      default=120, type=int,      help='set epochs')
-parser.add_argument('--re_epoch',   default=20, type=int,       help='set retrain epochs')
+parser.add_argument('--re_epoch',   default=60, type=int,       help='set retrain epochs')
 parser.add_argument('--num_sets',   default='8', type=int,      help='# of pattern sets')
 parser.add_argument('--exp',        default='test', type=str,   help='test or not')
-parser.add_argument('--l2',         default=True, action='store_true',  help='apply l2 regularization')
+parser.add_argument('--l2',         default=False, action='store_true', help='apply l3 regularization')
 parser.add_argument('--scratch',    default=False, action='store_true', help='start from pretrain/scratch')
 parser.add_argument('--no-cuda',    default=False, action='store_true', help='disables CUDA training')
 args = parser.parse_args()
 print(args)
-comment = "helloEveryone"
+comment = "check6"
 
 if args.exp == 'test':
     args.exp = f'{args.exp}-{time.strftime("%y%m%d-%H%M%S")}'
-args.save = f'logs/{args.dataset}/{args.model}/{args.exp}_lr{str(args.lr)}_{comment}'
-create_exp_dir(args.save)
+args.save = f'logs/{args.dataset}/{args.model}/{args.exp}_lr{str(args.lr)}_rls{str(args.re_lr)}_{comment}'
 
 args.workers = 16
 
@@ -116,10 +109,8 @@ np.random.seed(1)
 
 use_cuda = not args.no_cuda and torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
-########################################################################################
+##########################################################################################################
 
-
-##### Load Pre-trained Model #####
 print('Preparing pre-trained model...')
 if args.dataset == 'imagenet':
     pre_model = torchvision.models.vgg16(pretrained=True)
@@ -162,15 +153,14 @@ pre_model.cuda()
 
 
 # History collector
-history_score = np.zeros((args.epoch+1, 2))
+history_score = np.zeros((args.epoch+args.re_epoch+1, 2))
 
 
 # Optimizer
 optimizer = PruneAdam(model.named_parameters(), lr=args.lr, eps=args.adam_epsilon)
-# optimizer = optim.Adam(model.parameters(), args.lr)
 
 
-print('\nTraining...') ##### ##### ##### ##### #####
+print('\nTraining...') ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### #####
 best_prec = 0
 Z, Y, U, V = initialize_Z_Y_U_V(model)
 
@@ -191,11 +181,9 @@ for epoch in range(args.epoch):
     prec = test(args, model, device, test_loader)
     history_score[epoch][0] = epoch
     history_score[epoch][1] = prec
-
-    if prec > best_prec:
-        best_prec = prec
-        torch.save(model.state_dict(), os.path.join(args.save, 'cifar10_best.pth.tar'))
-
+    
+create_exp_dir(args.save)
+torch.save(model.state_dict(), os.path.join(args.save, 'cifar10_before.pth.tar'))
 
 
 # Real Pruning ! ! !
@@ -203,20 +191,63 @@ print("\nApply Pruning with connectivity & pattern set...")
 mask = apply_prune(args, model, device, pattern_set)
 print_prune(model)
 
+for name, param in model.named_parameters():
+    if name.split('.')[-1] == "weight" and len(param.shape)==4:
+        param.data.mul_(mask[name])
+
+torch.save(model.state_dict(), os.path.join(args.save, 'cifar10_after.pth.tar'))
+
 print("\ntesting...")
 test(args, model, device, test_loader)
 
+
 # Optimizer for Retrain
-optimizer = PruneAdam(model.named_parameters(), lr=args.lr, eps=args.adam_epsilon)
-# optimizer = optim.Adam(model.parameters(), args.lr)
+optimizer = PruneAdam(model.named_parameters(), lr=args.re_lr, eps=args.adam_epsilon)
 
+
+# Fine-tuning...
 print("\nfine-tuning...")
-retrain(args, model, mask, device, train_loader, test_loader, optimizer)
+best_prec = 0
+for epoch in range(args.re_epoch):
+    print("Epoch: {} with re_lr: {}".format(epoch+1, args.re_lr))
+    if epoch in [args.re_epoch//4, args.re_epoch//2, args.re_epoch//4*3]:
+        for param_group in optimizer.param_groups:
+            param_group['lr'] *= 0.1
 
-prec = test(args, model, device, test_loader)
-history_score[args.epoch][0] = 0
-history_score[args.epoch][1] = prec
+    retrain(args, model, mask, device, train_loader, test_loader, optimizer)
+
+    print("\ntesting...")
+    prec = test(args, model, device, test_loader)
+    history_score[args.epoch + epoch][0] = epoch
+    history_score[args.epoch + epoch][1] = prec
+    
+    if prec > best_prec:
+        best_prec = prec
+        torch.save(model.state_dict(), os.path.join(args.save, 'cifar10_pruned.pth.tar'))
+
 np.savetxt(os.path.join(args.save, 'train_record.txt'), history_score, fmt='%10.5f', delimiter=',')
+
+
+############################################
+
+# my mistake 1 - making mask.pickle
+"""
+with open('mask.pickle', 'wb') as fw:
+    pickle.dump(mask, fw)
+
+with open('mask.pickle', 'rb') as fr:
+    mask = pickle.load(fr)
+    print("mask loaded")
+"""
+
+# my mistake 2
+"""
+for module in model.named_modules():
+    if isinstance(module[1], nn.Conv2d):
+        print("module:", module[0])
+        prune.custom_from_mask(module, 'weight', mask=mask[module[0] +'.weight'])
+"""
+
 
 
 
