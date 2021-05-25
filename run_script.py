0c0b95a8f3054198286a8584f4d570111ce95f55
7bvcxz <7bvcxz@gmail.com>
2021-05-25 02:57:14 +0000

    main update!

--- run_script.py
+++ run_script.py
@@ -6,9 +6,7 @@ a = input()
 if a == '0':
     os.system("python3 main_loss3.py --lr 1e-4 --rho 2")
     os.system("python3 main_loss3.py --lr 1e-4 --rho 2.5")
-    os.system("python3 main_swp.py --lr 1e-4 --rho 3e-1")
 
 elif a == '1':
     os.system("python3 main_swp.py --lr 1e-4 --rho 1e-1")
-    os.system("python3 main_swp.py --lr 6e-5 --rho 3e-1")
     os.system("python3 main_swp.py --lr 6e-5 --rho 1e-1")
