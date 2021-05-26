c6c4b1555d87e036f4078c2c5962aa4cb42c9a90
7bvcxz <7bvcxz@gmail.com>
2021-05-26 02:32:30 +0000

    main update2

--- run_script.py
+++ run_script.py
@@ -1,12 +1,14 @@
 import os
 
-print('run script :')
+print('Enter run script num')
 a = input()
 
+print('run script', a)
+
 if a == '0':
-    os.system("python3 main_loss3.py --lr 1e-4 --rho 2")
-    os.system("python3 main_loss3.py --lr 1e-4 --rho 2.5")
+    os.system("python3 main_loss3.py --lr 1e-4 --rho 1e-1")
+    os.system("python3 main_loss3.py --lr 1e-4 --rho 6")
 
 elif a == '1':
-    os.system("python3 main_swp.py --lr 1e-4 --rho 1e-1")
-    os.system("python3 main_swp.py --lr 6e-5 --rho 1e-1")
+    os.system("python3 main_loss3.py --lr 1e-4 --rho 10")
+    os.system("python3 main_loss3.py --lr 6e-5 --rho 1e-1")
